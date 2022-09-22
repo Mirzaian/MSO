1a00007a0c175bfe05a448c072610d7b7290aa5d
Kevin Mirzaian <kevin@mirzaian.de>
2022-09-22 18:39:21 +0200

    footer loadTranslation added

--- src/app/components/overlay/footer/footer.component.ts
+++ src/app/components/overlay/footer/footer.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, NgModule, CUSTOM_ELEMENTS_SCHEMA, OnInit, Output, NgZone } from '@angular/core';
+import { Component, Input, NgModule, CUSTOM_ELEMENTS_SCHEMA, OnInit, Output, NgZone, ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
@@ -8,16 +9,32 @@ import { Router } from '@angular/router';
 })
 export class FooterComponent implements OnInit {
 
+  currentLanguage: string = 'de-DE';
+
   footerNavigation = [
-    { name: "Contact", id: "Contact", onClick: () => { this.ngZone.run(() => { this.router.navigate(['/contact']) }) } },
-    { name: "Terms and conditions", id: "Terms and conditions", href: "#terms-and-conditions"},
-    { name: "Legal notice", id: "Legal notice", href: "#legal-notice"},
-    { name: "Data privacy", id: "Data privacy", href: "#data-privacy", icon: "alert-imprint-dataprivacy"},
+    { name: "Kontakt", id: "Contact", onClick: () => { this.ngZone.run(() => { this.router.navigate(['/contact']) }) } },
+    { name: "Nutzungsbedinungen", id: "Terms and conditions", onClick: () => { this.ngZone.run(() => { this.router.navigate(['/contact']) }) }},
+    { name: "Impressum", id: "Legal notice", onClick: () => { this.ngZone.run(() => { this.router.navigate(['/contact']) }) }},
+    { name: "Datenschutz", id: "Data privacy", icon: "alert-imprint-dataprivacy", onClick: () => { this.ngZone.run(() => { this.router.navigate(['/contact']) }) }},
   ];
 
-  constructor(private router: Router, private ngZone: NgZone) { }
+  constructor(private router: Router, private ngZone: NgZone, private cdf: ChangeDetectorRef, private translateService: TranslateService) {
+    this.translateService.getTranslation(this.currentLanguage).subscribe(() => { this.loadTranslations() })
+    translateService.onLangChange.subscribe((languageSettings) => {
+      this.currentLanguage = languageSettings.lang;
+      this.loadTranslations();
+      this.cdf.markForCheck();
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  loadTranslations() {
+    this.footerNavigation[0].name = this.translateService.instant("footer.contact");
+    this.footerNavigation[1].name = this.translateService.instant("footer.terms");
+    this.footerNavigation[2].name = this.translateService.instant("footer.lg");
+    this.footerNavigation[3].name = this.translateService.instant("footer.dp");
+  }
 }
+
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -15,7 +15,7 @@ export class LoginComponent implements OnInit {
   ngOnInit() { }
 
   login() {
-    if (this.email == "k.mirzaian@telekom.de" && this.password == "123456") {
+    if (this.email == "root" && this.password == "123456") {
       this.ngZone.run(() => { this.router.navigate(['dashboard']) })
       this.userService.isUserLoggedIn$.next(true)
       localStorage.setItem('loginStatus', 'true')
